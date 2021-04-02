d7ee9e5e8d4686dc99f11a634ac2d9833e3f043c
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-04-02 20:07:58 -0300

    Adicionado o chechmark nas div de cada item

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function selecionado(parametro){
     const item = document.querySelector(parametro);
+    const check = document.querySelector(parametro + " .checkmark");
     item.classList.toggle("item-selecionado");
+    check.classList.toggle("esconder");
 }
