6406462bb467863b73fca41c2fd73a5b4bd141ea
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-04-02 22:29:02 -0300

    Adicionado a seleçao exclusiva

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+function deselecionar(parametro){
+    const itempre = document.querySelector(parametro + " .item-selecionado");
+    const checkpre = document.querySelector(parametro + " .item-selecionado" + " .checkmark");
+    if(itempre !== null){
+        itempre.classList.remove("item-selecionado");
+        checkpre.classList.add("esconder");
+    }
+
+}
+
 function selecionado(parametro){
     const item = document.querySelector(parametro);
     const check = document.querySelector(parametro + " .checkmark");
